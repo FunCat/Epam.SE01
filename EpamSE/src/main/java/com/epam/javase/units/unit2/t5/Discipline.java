748c0ce796e5fae78d0af860d1b98ac95645fb69
FunCat <shadow170713@gmail.com>
2017-05-13 18:55:43 +0300

    Unit-2 Last Version

--- EpamSE/src/main/java/com/epam/javase/units/unit2/t5/Discipline.java
+++ EpamSE/src/main/java/com/epam/javase/units/unit2/t5/Discipline.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 public class Discipline {
     private String name;
-<<<<<<< HEAD
     private Map<Student, Float> listStudents = new HashMap<>();
     private static boolean isInt = true;
 
@@ -22,20 +21,6 @@ public class Discipline {
     /** Generate a random mark from 2 to 5*/
     public static float generateMark(){
         return (isInt)?(int)(Math.random() * 3 + 2):(float)(Math.random() * 3 + 2);
-=======
-    private Map<Student, Float> listStudents = new HashMap<Student, Float>();
-    private boolean isInt;
-
-    public Discipline(String name, boolean isInt) {
-        this.name = name;
-        listStudents = new HashMap<Student, Float>();
-        this.isInt = isInt;
-    }
-
-    /** Generate a random mark from 2 to 5*/
-    public float generateMark(){
-        return (isInt)?((int)(Math.random() * 3 + 2)):((float)(Math.random() * 3 + 2));
->>>>>>> 7e4a53cf7088a245feb1c7e52decbaf4684feb4f
     }
 
     /** Add a student in the discipline map */
@@ -46,26 +31,11 @@ public class Discipline {
         listStudents.put(student, mark);
     }
 
-    /** Find a maxValue from all selected maps, if a student doesn't contain in anywhere maps return 0 */
-    public float findStudentInDiscipline(Student student, Map<Student, Float>... listesStudents){
-        float maxValue = 0;
-        for(int i = 0; i < listesStudents.length; i++) {
-            if (listesStudents[i].containsKey(student)) {
-                if(maxValue < listesStudents[i].get(student)) maxValue = listesStudents[i].get(student);
-            }
-        }
-        return maxValue;
-    }
-
     @Override
     public String toString() {
         String text = "Discipline " + name + "\n";
         for(Map.Entry<Student, Float> student : listStudents.entrySet()){
-<<<<<<< HEAD
             text += "\t" + student.getKey() + " " + strFormatFloat(student.getValue()) + "\n";
-=======
-            text += "\t" + student.getKey() + " " + String.format("%.2f",student.getValue()) + "\n";
->>>>>>> 7e4a53cf7088a245feb1c7e52decbaf4684feb4f
         }
         return text;
     }
@@ -80,14 +50,9 @@ public class Discipline {
         Student st7 = new Student("Sergey", "Sverev", 1995);
 
         Discipline math = new Discipline("Math", false);
-<<<<<<< HEAD
         math.addNewStudentForDiscipline(st1, generateMark());
         math.addNewStudentForDiscipline(st5, generateMark());
         math.addNewStudentForDiscipline(st3, generateMark());
-=======
-        math.addNewStudentForDiscipline(st1);
-        math.addNewStudentForDiscipline(st3);
->>>>>>> 7e4a53cf7088a245feb1c7e52decbaf4684feb4f
         System.out.println(math);
 
         Discipline english = new Discipline("English", true);
@@ -109,7 +74,6 @@ public class Discipline {
 
     }
 
-<<<<<<< HEAD
     /** Find student in all choose disciplines, and if find the student return the biggest Mark otherwise return 0.0f*/
     public static float findStudentInDiscipline(Student student, Discipline... disciplines){
         float maxMark = 0.0f;
@@ -126,9 +90,6 @@ public class Discipline {
     public static String strFormatFloat(float num){
         return String.format("%.2f", num);
     }
-=======
-
->>>>>>> 7e4a53cf7088a245feb1c7e52decbaf4684feb4f
 
 
 }
