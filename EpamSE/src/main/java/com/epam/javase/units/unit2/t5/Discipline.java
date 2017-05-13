7e4a53cf7088a245feb1c7e52decbaf4684feb4f
FunCat <shadow170713@gmail.com>
2017-05-13 17:02:43 +0300

    Unit-2 Task-5 update

--- EpamSE/src/main/java/com/epam/javase/units/unit2/t5/Discipline.java
+++ EpamSE/src/main/java/com/epam/javase/units/unit2/t5/Discipline.java
@@ -5,17 +5,19 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Discipline {
-    String name;
-    Map<Student, Integer> listStudents = new HashMap<Student, Integer>();
+    private String name;
+    private Map<Student, Float> listStudents = new HashMap<Student, Float>();
+    private boolean isInt;
 
-    public Discipline(String name) {
+    public Discipline(String name, boolean isInt) {
         this.name = name;
-        listStudents = new HashMap<Student, Integer>();
+        listStudents = new HashMap<Student, Float>();
+        this.isInt = isInt;
     }
 
     /** Generate a random mark from 2 to 5*/
-    public int generateMark(){
-        return (int)(Math.random() * 3 + 2);
+    public float generateMark(){
+        return (isInt)?((int)(Math.random() * 3 + 2)):((float)(Math.random() * 3 + 2));
     }
 
     /** Add a student in the discipline map */
@@ -23,11 +25,22 @@ public class Discipline {
         listStudents.put(student, generateMark());
     }
 
+    /** Find a maxValue from all selected maps, if a student doesn't contain in anywhere maps return 0 */
+    public float findStudentInDiscipline(Student student, Map<Student, Float>... listesStudents){
+        float maxValue = 0;
+        for(int i = 0; i < listesStudents.length; i++) {
+            if (listesStudents[i].containsKey(student)) {
+                if(maxValue < listesStudents[i].get(student)) maxValue = listesStudents[i].get(student);
+            }
+        }
+        return maxValue;
+    }
+
     @Override
     public String toString() {
         String text = "Discipline " + name + "\n";
-        for(Map.Entry<Student, Integer> student : listStudents.entrySet()){
-            text += "\t" + student.getKey() + " " + student.getValue() + "\n";
+        for(Map.Entry<Student, Float> student : listStudents.entrySet()){
+            text += "\t" + student.getKey() + " " + String.format("%.2f",student.getValue()) + "\n";
         }
         return text;
     }
@@ -41,15 +54,13 @@ public class Discipline {
         Student st6 = new Student("Maksim", "Gorbachov", 1997);
         Student st7 = new Student("Sergey", "Sverev", 1995);
 
-        Discipline math = new Discipline("Math");
+        Discipline math = new Discipline("Math", false);
         math.addNewStudentForDiscipline(st1);
         math.addNewStudentForDiscipline(st3);
         System.out.println(math);
 
     }
 
-    public void findStudentInDiscipline(){
 
-    }
 
 }
